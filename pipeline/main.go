28edd260351037272d57be171d7e0c9b19ad8182
mlabouardyfox <mohamed@foxintelligence.fr>
2020-04-15 12:25:22 +0200

    get current environment

--- pipeline/main.go
+++ pipeline/main.go
@@ -1,10 +1,23 @@
 package main
 
 import (
+	"encoding/json"
 	"log"
 	"net/http"
+	"os"
 	"path"
 )
+
+type Response struct {
+	Environment string `json:"environment"`
+}
+
+func EnvironmentHandler(w http.ResponseWriter, r *http.Request) {
+	response := Response{
+		Environment: os.Getenv("ENVIRONMENT"),
+	}
+	json.NewEncoder(w).Encode(response)
+}
 
 func IndexHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Add("Content Type", "text/html")
@@ -13,6 +26,7 @@ func IndexHandler(w http.ResponseWriter, r *http.Request) {
 
 func main() {
 	http.HandleFunc("/", IndexHandler)
+	http.HandleFunc("/environment", EnvironmentHandler)
 	if err := http.ListenAndServe(":3000", nil); err != nil {
 		log.Fatal(err)
 	}
